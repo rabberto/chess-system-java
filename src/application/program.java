48f5ba8b05874cee15978831cd59ac2f813a076e
rabberto <rafael.rbberto@gmail.com>
2020-09-15 14:17:58 -0300

    First class: Position

---
+++ src/application/program.java
@@ -0,0 +1,14 @@
+package application;
+
+import boardgame.Position;
+
+public class program {
+
+	public static void main(String[] args) {
+	
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
+
+	}
+
+}
